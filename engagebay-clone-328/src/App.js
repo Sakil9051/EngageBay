363398746c762e8ee02a7714574b2046817b19b0
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2022-11-09 19:33:54 +0530

    Navbar sticky

--- engagebay-clone-328/src/App.js
+++ engagebay-clone-328/src/App.js
@@ -4,9 +4,11 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import DashNavbar from './Components/DashNav';
 import Home from "./Pages/Home/Home.jsx"
+import VideoBackGround from './Pages/Home/VideoBackGround';
 function App() {
   return (
     <div className="App">
+        <Home/>
 
     </div>
   );
--- engagebay-clone-328/src/Pages/Home/Home.jsx
+++ engagebay-clone-328/src/Pages/Home/Home.jsx
@@ -4,10 +4,12 @@ import './Home.css';
 import {Box} from "@chakra-ui/react";
 import AvtivitySolution from "./AvtivitySolution";
 import VideoBackGround from "./VideoBackGround.jsx"
+import Navbar from "../../Components/Navbar";
 function Home() {
   return (
     <>
       <Box id="nav-container" w={"full"}>
+        <Navbar/>
       </Box>
       <Box w={"full"} overflow={"hidden"}>
         <VideoBackGround/>
