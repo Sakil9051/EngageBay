a071eaedc83647168270765d560b3021d1d9a401
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2022-11-10 18:14:01 +0530

    dashboard home page layout done

--- engagebay-clone-328/src/App.js
+++ engagebay-clone-328/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import DashNavbar from './Components/DashNav';
-import Home from "./Pages/Home/Home.jsx"
-import VideoBackGround from './Pages/Home/VideoBackGround';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import DashNavbar from "./Components/DashNav";
+import Home from "./Pages/Home/Home.jsx";
+import VideoBackGround from "./Pages/Home/VideoBackGround";
+import Dashboard from "./Components/Dashboard";
 function App() {
   return (
     <div className="App">
-        <Home/>
-
+     
     </div>
   );
 }
