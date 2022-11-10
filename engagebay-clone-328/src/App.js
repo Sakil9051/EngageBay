63a8583946f92ffd78d864e9c61de3c43f01ff91
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-11-10 10:51:38 +0530

    2nd commit

--- engagebay-clone-328/src/App.js
+++ engagebay-clone-328/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Login from "./Login_And_Signup/Login"
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Footer/>
+      {/* <Navbar/> */}
+      {/* <Footer/> */}
+      <Login/>
     </div>
   );
 }
