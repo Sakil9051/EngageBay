a856c70e32837e34580f0f8f087ce92b955454dd
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-11-11 02:36:22 +0530

    login Signup functionality done2

--- engagebay-clone-328/src/App.js
+++ engagebay-clone-328/src/App.js
@@ -6,9 +6,14 @@ import DashNavbar from "./Components/DashNav";
 import Home from "./Pages/Home/Home.jsx";
 import VideoBackGround from "./Pages/Home/VideoBackGround";
 import Dashboard from "./Components/Dashboard";
+import Allroutes from "./Routes/Allroutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Allroutes />
+    </div>
+  );
 }
 
 export default App;
