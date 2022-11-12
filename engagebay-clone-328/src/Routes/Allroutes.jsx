d072982a44223b1d821d9f5f9aecc0a580b7c560
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-11-12 11:24:06 +0530

    templates_reset_password3

--- engagebay-clone-328/src/Routes/Allroutes.jsx
+++ engagebay-clone-328/src/Routes/Allroutes.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import { UserAuthContextProvider } from '../Context/userAuthContext';
 import Home from "../Pages/Home/Home";
-import Login from "../Pages/loginAndSignup/Login";
+import Login from "../Pages/loginAndSignup/Login"
+import Signup from '../Pages/loginAndSignup/Signup';
 import Resetpassword from "../Pages/loginAndSignup/Resetpassword";
 import Signup from "../Pages/loginAndSignup/Signup";
 
