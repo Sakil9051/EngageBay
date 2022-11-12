d01ef5087fcde56090122c9a16f30c8382a4ba3c
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-11-12 11:33:39 +0530

    tempates_reset_password5

--- engagebay-clone-328/src/Routes/Allroutes.jsx
+++ engagebay-clone-328/src/Routes/Allroutes.jsx
@@ -4,20 +4,21 @@ import { UserAuthContextProvider } from '../Context/userAuthContext';
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/loginAndSignup/Login"
 import Signup from '../Pages/loginAndSignup/Signup';
-import Resetpassword from "../Pages/loginAndSignup/Resetpassword";
+import Resetpassword from '../Pages/loginAndSignup/Resetpassword';
 const Allroutes = () => {
   return (
     <div>
       <UserAuthContextProvider>
+
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/resetpassword" element={<Resetpassword />} />
+            <Route path='/' element={<Home/> } />
+            <Route path='/login' element={<Login/> } />
+            <Route path='/signup' element={<Signup/> } />
+            <Route path='/resetpassword' element={<Resetpassword/> } />
         </Routes>
       </UserAuthContextProvider>
     </div>
-  );
-};
+  )
+}
 
-export default Allroutes;
+export default Allroutes
