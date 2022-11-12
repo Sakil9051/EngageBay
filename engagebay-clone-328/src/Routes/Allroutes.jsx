e59e157141a29492739eb952411d6357548d601e
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-11-12 11:45:04 +0530

    8th

--- engagebay-clone-328/src/Routes/Allroutes.jsx
+++ engagebay-clone-328/src/Routes/Allroutes.jsx
@@ -4,6 +4,7 @@ import { UserAuthContextProvider } from '../Context/userAuthContext';
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/loginAndSignup/Login"
 import Signup from '../Pages/loginAndSignup/Signup';
+import Resetpassword from '../Pages/loginAndSignup/Resetpassword';
 const Allroutes = () => {
   return (
     <div>
@@ -13,6 +14,7 @@ const Allroutes = () => {
             <Route path='/' element={<Home/> } />
             <Route path='/login' element={<Login/> } />
             <Route path='/signup' element={<Signup/> } />
+            <Route path='/resetpassword' element={<Resetpassword/> } />
         </Routes>
       </UserAuthContextProvider>
     </div>
