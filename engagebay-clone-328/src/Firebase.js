6ec8bb0d57a16954aa30305718d89688d705b845
Sahil Laskar <105960136+Sakil9051@users.noreply.github.com>
2022-12-11 17:01:43 +0530

    Update Firebase.js

--- engagebay-clone-328/src/Firebase.js
+++ engagebay-clone-328/src/Firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCzg_Vh0E-8lmnsrUDDdkDOaJFmr8wH_Qg",
-  authDomain: "engagebay-clone-authintication.firebaseapp.com",
-  projectId: "engagebay-clone-authintication",
-  storageBucket: "engagebay-clone-authintication.appspot.com",
-  messagingSenderId: "849756603450",
-  appId: "1:849756603450:web:a58889809b2b57b161e103",
-  measurementId: "G-ZYG99GXVSG",
+  apiKey: "AIzaSyDnyrSumXSCbLT2-9uKu2J6nzmLqKt2Ou8",
+  authDomain: "engagebay-370811.firebaseapp.com",
+  projectId: "engagebay-370811",
+  storageBucket: "engagebay-370811.appspot.com",
+  messagingSenderId: "1064095341723",
+  appId: "1:1064095341723:web:5577cc1a8869e4786c3a3e"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth =getAuth(app)
 export default app
